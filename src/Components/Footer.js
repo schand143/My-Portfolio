35f880f7b336200ba70a0441ecb7fa3c4fe17543
schand143 <shaikhchand724@gmail.com>
2019-12-25 22:39:56 +0530

    Project deployed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,12 +22,6 @@ class Footer extends Component {
 
             <ul className="copyright">
               <li>&copy; Copyright 2019 Chand Shaikh</li>
-              <li>
-                Design by{" "}
-                <a title="Styleshout" href="http://www.styleshout.com/">
-                  Styleshout
-                </a>
-              </li>
             </ul>
           </div>
           <div id="go-top">
