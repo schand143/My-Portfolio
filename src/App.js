6e51895cf379a15a74c47f1593f3ba4b80367e34
schand143 <shaikhchand724@gmail.com>
2019-12-30 23:17:11 +0530

    Path

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: "./resumeData.json",
+      url: "/My-Portfolio/resumeData",
       dataType: "json",
       cache: false,
       success: function(data) {
