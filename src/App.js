756f29385f8bd7992b8706b4914758a3cc72a0a9
appasaheb4 <onlyappasaheb4@gmail.com>
2019-12-31 21:40:31 +0530

    json issue solve.

--- src/App.js
+++ src/App.js
@@ -10,47 +10,43 @@ import Contact from "./Components/Contact";
 import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import resumeData from "./resumeData.json";
+
+class App extends Component
+{
+  constructor ( props )
+  {
+    super( props );
     this.state = {
       foo: "bar",
       resumeData: {}
     };
 
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
+    ReactGA.initialize( "UA-110570651-1" );
+    ReactGA.pageview( window.location.pathname );
   }
 
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
+  getResumeData ()
+  {
+    this.setState( { resumeData: resumeData } );
   }
 
-  componentDidMount() {
+  componentDidMount ()
+  {
     this.getResumeData();
   }
 
-  render() {
+  render ()
+  {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Testimonials data={this.state.resumeData.testimonials} />
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
+        <Header data={ this.state.resumeData.main } />
+        <About data={ this.state.resumeData.main } />
+        <Resume data={ this.state.resumeData.resume } />
+        <Portfolio data={ this.state.resumeData.portfolio } />
+        <Testimonials data={ this.state.resumeData.testimonials } />
+        <Contact data={ this.state.resumeData.main } />
+        <Footer data={ this.state.resumeData.main } />
       </div>
     );
   }
