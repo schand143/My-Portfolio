e8a7b5620d6a2db555e946846a5c5975581191ac
schand143 <shaikhchand724@gmail.com>
2020-03-02 22:30:36 +0530

    Link added

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import $ from "jquery";
-import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Testimonials from "./Components/Testimonials";
-import Portfolio from "./Components/Portfolio";
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
 // json file import
-import resumeData from "./resumeData.json";
+import resumeData from './resumeData.json';
 
-class App extends Component
-{
-  constructor ( props )
-  {
-    super( props );
+class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      foo: "bar",
+      foo: 'bar',
       resumeData: {}
     };
 
-    ReactGA.initialize( "UA-110570651-1" );
-    ReactGA.pageview( window.location.pathname );
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
   }
 
-  getResumeData ()
-  {
-    this.setState( { resumeData: resumeData } );
+  getResumeData() {
+    this.setState({ resumeData: resumeData });
   }
 
-  componentDidMount ()
-  {
+  componentDidMount() {
     this.getResumeData();
   }
 
-  render ()
-  {
+  render() {
     return (
-      <div className="App">
-        <Header data={ this.state.resumeData.main } />
-        <About data={ this.state.resumeData.main } />
-        <Resume data={ this.state.resumeData.resume } />
-        <Portfolio data={ this.state.resumeData.portfolio } />
-        <Testimonials data={ this.state.resumeData.testimonials } />
-        <Contact data={ this.state.resumeData.main } />
-        <Footer data={ this.state.resumeData.main } />
+      <div className='App'>
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Testimonials data={this.state.resumeData.testimonials} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
       </div>
     );
   }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class About extends Component {
   render() {
     if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic = "images/" + this.props.data.image;
+      var profilepic = 'images/' + this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -17,23 +17,23 @@ class About extends Component {
     }
 
     return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
+      <section id='about'>
+        <div className='row'>
+          <div className='three columns'>
             <img
-              className="profile-pic"
+              className='profile-pic'
               src={profilepic}
-              alt="Chand Shaikh Profile Pic"
+              alt='Chand Shaikh Profile Pic'
             />
           </div>
-          <div className="nine columns main-col">
+          <div className='nine columns main-col'>
             <h2>About Me</h2>
 
             <p>{bio}</p>
-            <div className="row">
-              <div className="columns contact-details">
+            <div className='row'>
+              <div className='columns contact-details'>
                 <h2>Contact Details</h2>
-                <p className="address">
+                <p className='address'>
                   <span>{name}</span>
                   <br />
                   <span>
@@ -47,10 +47,10 @@ class About extends Component {
                   <span>{email}</span>
                 </p>
               </div>
-              <div className="columns download">
+              <div className='columns download'>
                 <p>
-                  <a href={resumeDownload} className="button">
-                    <i className="fa fa-download"></i>Download Resume
+                  <a href={resumeDownload} className='button'>
+                    <i className='fa fa-download'></i>Download Resume
                   </a>
                   {/* <img className="button" src={cv} alt="Chand Shaikh Resume" /> */}
                 </p>
